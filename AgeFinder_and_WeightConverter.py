b45a2b03bca33eac567db7fc9577428ca9798107
KAILAASH KUMAR P V <62185391+KAILAASHKUMAR2841@users.noreply.github.com>
2022-11-01 12:48:32 +0530

    Update AgeFinder_and_WeightConverter.py

--- AgeFinder_and_WeightConverter.py
+++ AgeFinder_and_WeightConverter.py
@@ -4,6 +4,6 @@ your_age = current_year - int(birth_year)
 print(type(your_age))
 print("Your age is: "+ str(your_age))
 
-kg_weight = input("Weight in pounds: ")
+kg_weight = input("Weight in kilos: ")
 lb_weight = int(kg_weight) / 0.45359237
 print("Your weight in pounds: "+lb_weight)
