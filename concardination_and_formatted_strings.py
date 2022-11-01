0adadf5bcd5eeb92dedb74430ed647bb4a7a4ae6
KAILAASH KUMAR P V <62185391+KAILAASHKUMAR2841@users.noreply.github.com>
2022-11-01 12:47:49 +0530

    Create concardination_and_formatted_strings.py

---
+++ concardination_and_formatted_strings.py
@@ -0,0 +1,7 @@
+fname = "Kailaash"
+lname = "Kumar"
+#output: Kailaash [Kumar] is a coder!
+concardination = fname + " ["+lname+"] is a coder!"
+formatted = f"{fname} [{lname}] is a coder!"
+print (concardination)
+print(formatted)
