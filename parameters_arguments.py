856b360735534c7729e8536111b039cc6759e705
KAILAASH KUMAR P V <62185391+KAILAASHKUMAR2841@users.noreply.github.com>
2022-11-04 00:33:28 +0530

    Create parameters_arguments.py

---
+++ parameters_arguments.py
@@ -0,0 +1,9 @@
+def greet_user(name,number,sex):  #parameter
+  print(f"Hello {name}")
+  print(f"Your {number} and {sex}")
+print("Start")
+n=input("NAME: ")
+nu=input("NUMBER: ")
+s=input("SEX: ")
+greet_user(n,nu,s)
+print("Finish")        
