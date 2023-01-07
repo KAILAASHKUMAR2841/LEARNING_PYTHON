b905bb2b67c9a5e410c3f5c9c0805045993eed36
KAILAASH KUMAR P V <62185391+KAILAASHKUMAR2841@users.noreply.github.com>
2023-01-07 14:00:58 +0530

    Add files via upload

---
+++ python files/images.py
@@ -0,0 +1,6 @@
+from utils import find_max
+
+number = input("Enter the number: ").split()
+max = find_max(number)
+print(max) 
+
---
+++ python files/utils.py
@@ -0,0 +1,6 @@
+def find_max(number):
+  max = number[0]
+  for i in number:
+    if i > max:
+      max = i
+  return (max)
